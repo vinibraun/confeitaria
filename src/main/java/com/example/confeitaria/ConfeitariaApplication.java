c24ee83332ecdb9a843776868bf6ebe6c8cbff76
vini <braunvinicius2@gmail.com>
2022-05-06 19:18:34 -0300

    Initial commit

---
+++ src/main/java/com/example/confeitaria/ConfeitariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.confeitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfeitariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfeitariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/confeitaria/ConfeitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.confeitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
